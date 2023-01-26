4b077fe226fedd766325e362230999160798d1cc
Dylan Shevlin <Dylanshevlinn@gmail.com>
2023-01-26 23:39:09 +0000

    Working on NPC interactions

---
+++ Assets/Game/Scripts/PlayerInteractions/ChatBubble.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChatBubble : MonoBehaviour
+{
+    private SpriteRenderer backgroundSpriteRenderer;
+    private TextMeshPro textMeshPro;
+    
+    private void Awake()
+    {
+        backgroundSpriteRenderer = transform.Find("Background").GetComponent<SpriteRenderer>();
+        textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
+    }
+
+    private void Setup(string text)
+    {
+        textMeshPro.SetText(text);
+        textMeshPro.ForceMeshUpdate();
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+
+        Vector2 padding = new Vector2(4f, 2f);
+        backgroundSpriteRenderer.size = textSize + padding;
+    }
+   
+}
---
+++ Assets/Game/Scripts/PlayerInteractions/NPCInteractable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteractable : MonoBehaviour
+{
+    
+    public void Interact()
+    {
+        ChatBubble3D.Create(transform.transform, new Vector3(-.3f, 1.7f, 0f), ChatBubble3D.IconType.Happy, "Hello there!");
+    }
+}
