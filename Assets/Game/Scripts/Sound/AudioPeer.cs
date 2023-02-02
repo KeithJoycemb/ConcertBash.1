33d66c24825423f496c4bf1725eb60e5724fbd3e
Patrick-A47 <72445293+Patrick-A47@users.noreply.github.com>
2023-02-02 00:41:56 +0000

    Created frequency bands
    These frequency bands react to sound in a similar way to the audio visualizer.

--- Assets/Game/Scripts/Sound/AudioPeer.cs
+++ Assets/Game/Scripts/Sound/AudioPeer.cs
@@ -7,6 +7,9 @@ public class AudioPeer : MonoBehaviour
 
     AudioSource _audioSource;
     public static float[] _samples = new float[512];
+    public static float[] freqBand = new float[8];
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +20,36 @@ public class AudioPeer : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
+        MakeFrequencyBands();
     }
 
     void GetSpectrumAudioSource()
     {
         _audioSource.GetSpectrumData(_samples, 0,FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        for (int i = 0;i<8;i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for(int j = 0;j<sampleCount;j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+            average /= count;
+
+            freqBand[i] = average * 10;
+        }
+
     }
 }
